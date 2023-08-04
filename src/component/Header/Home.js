12ebd3a8620998a945d7847dc36510a84e2bf5b9
Elaina Rohlfing <emrwc6@umsystem.edu>
2023-08-04 17:05:32 -0500

    homeClickHandler fixed to capture id of button

--- src/component/Header/Home.js
+++ src/component/Header/Home.js
@@ -2,10 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from './Home.module.css';
 import {faHome} from '@fortawesome/free-solid-svg-icons';
 
-function Home({onClick}) {
+function Home({onHomeClick}) {
     return (
         <div>
-            <FontAwesomeIcon className={classes.home} icon={faHome} onClick={onClick}/>
+            <FontAwesomeIcon className={classes.home} icon={faHome} onClick={onHomeClick} id='Home'/>
         </div>
     )
 }
